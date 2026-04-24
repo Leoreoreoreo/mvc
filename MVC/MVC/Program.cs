22de0aa8d4e0d22dab0cb92bce202714230feaee
Nicholas Nicholas <nickolaosnick@gmail.com>
2026-04-24 12:11:23 +0100

    set up solution

---
+++ MVC/MVC/Program.cs
@@ -0,0 +1,24 @@
+
+namespace MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                _ = endpoints.MapControllers();
+            });
+
+            app.Run();
+        }
+    }
+}
